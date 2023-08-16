c1ac0dccd0c5a8c83f168ef5e9fc278e9f0dc016
Gttolla <lucaspedrosupergeeks@gmail.com>
2023-08-15 23:58:03 -0300

    Project configuration

---
+++ frontend/app/index.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Hello World</Text>
+      <Text>This is the first page of your app.</Text>
+    </View>
+  );
+}
+
+
